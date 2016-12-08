ad16798e02cd0c3e572f7f337ce1f954bd0713d7
Gregg Hernandez <gregg@lucidchart.com>
2016-12-08 09:59:27 -0700

    Set proper indentation for closing brackets preceded by a blank line

--- kotlin-mode.el
+++ kotlin-mode.el
@@ -241,7 +241,7 @@
       (progn
         (kotlin-mode--beginning-of-buffer-indent))
     (let ((not-indented t) cur-indent)
-      (cond ((looking-at "^[ \t]*\\.")
+      (cond ((looking-at "^[ \t]*\\.") ; line starts with .
              (save-excursion
                (kotlin-mode--prev-line)
                (cond ((looking-at "^[ \t]*\\.")
@@ -252,16 +252,19 @@
                (if (< cur-indent 0)
                    (setq cur-indent 0))))
 
-            ((looking-at "^[ \t]*}")
+            ((looking-at "^[ \t]*}") ; line starts with }
              (save-excursion
                (kotlin-mode--prev-line)
-               (while (and (looking-at "^[ \t]*\\.") (not (bobp)))
+               (while (and (or (looking-at "^[ \t]*$") (looking-at "^[ \t]*\\.")) (not (bobp)))
                  (kotlin-mode--prev-line))
-               (setq cur-indent (- (current-indentation) kotlin-tab-width)))
+               (cond ((or (looking-at ".*{[ \t]*$") (looking-at ".*{.*->[ \t]*$"))
+                      (setq cur-indent (current-indentation)))
+                     (t
+                      (setq cur-indent (- (current-indentation) kotlin-tab-width)))))
              (if (< cur-indent 0)
                  (setq cur-indent 0)))
 
-            ((looking-at "^[ \t]*)")
+            ((looking-at "^[ \t]*)") ; line starts with )
              (save-excursion
                (kotlin-mode--prev-line)
                (setq cur-indent (- (current-indentation) (* 2 kotlin-tab-width))))
@@ -272,23 +275,23 @@
              (save-excursion
                (while not-indented
                  (kotlin-mode--prev-line)
-                 (cond ((looking-at ".*{[ \t]*$") ; 4.)
+                 (cond ((looking-at ".*{[ \t]*$") ; line ends with {
                         (setq cur-indent (+ (current-indentation) kotlin-tab-width))
                         (setq not-indented nil))
 
-                       ((looking-at "^[ \t]*}") ; 3.)
+                       ((looking-at "^[ \t]*}") ; line starts with }
                         (setq cur-indent (current-indentation))
                         (setq not-indented nil))
 
-                       ((looking-at ".*{.*->[ \t]*$")
+                       ((looking-at ".*{.*->[ \t]*$") ; line ends with ->
                         (setq cur-indent (+ (current-indentation) kotlin-tab-width))
                         (setq not-indented nil))
 
-                       ((looking-at ".*([ \t]*$")
+                       ((looking-at ".*([ \t]*$") ; line ends with (
                         (setq cur-indent (+ (current-indentation) (* 2 kotlin-tab-width)))
                         (setq not-indented nil))
 
-                       ((looking-at "^[ \t]*).*$")
+                       ((looking-at "^[ \t]*).*$") ; line starts with )
                         (setq cur-indent (current-indentation))
                         (setq not-indented nil))
 
